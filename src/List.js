a6496f9c87a9c2ed9a83c04511855e6fd1021d79
sistemas <sistemas@expertic.es>
2021-05-24 17:48:39 +0200

    Pue eso

---
+++ src/List.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function List(props){
+    return(
+        <h1>Que tal estamos</h1>
+    );
+}
+
+export default List;
---
+++ src/Menu.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Menu(props){
+    return(
+        <h1>Hola {props.nombre}</h1>
+    );
+}
+
+export default Menu;
